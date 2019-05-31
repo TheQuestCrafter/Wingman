99b30e978a2f9fd04c7435ff356ec0b1b18b1862
CalvinOh <calvinohwyekit@gmail.com>
2019-05-30 22:14:22 -0500

    Working GenericNPC & Spawner

---
+++ Assets/Scripts/AI/Generic/GenericNPCMovement.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenericNPCMovement : MonoBehaviour
+{
+    [SerializeField]
+    float speed;
+    [SerializeField]
+    float waitTimeLength;
+    [SerializeField]
+    float waitUntil;
+    [SerializeField]
+    Vector2 destinationLocation;
+    [SerializeField]
+    public GameObject fieldOfLocation;
+
+    void Start()
+    {
+        destinationLocation = new Vector2(fieldOfLocation.GetComponent<BoxCollider2D>().transform.position.x + Random.Range(-fieldOfLocation.GetComponent<BoxCollider2D>().size.x / 2, fieldOfLocation.GetComponent<BoxCollider2D>().size.x / 2), fieldOfLocation.GetComponent<BoxCollider2D>().transform.position.y + Random.Range(-fieldOfLocation.GetComponent<BoxCollider2D>().size.y / 2, fieldOfLocation.GetComponent<BoxCollider2D>().size.y / 2));
+        waitUntil = Time.time + waitTimeLength;
+    }
+    void Update()
+    {
+        Movement();
+    }
+
+    void Movement()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, destinationLocation, speed * Time.deltaTime);
+   
+        if (Time.time >= waitUntil)
+        {
+            Debug.Log("Moving to Target");
+            destinationLocation = new Vector2(fieldOfLocation.GetComponent<BoxCollider2D>().transform.position.x + Random.Range(-fieldOfLocation.GetComponent<BoxCollider2D>().size.x / 2, fieldOfLocation.GetComponent<BoxCollider2D>().size.x / 2), fieldOfLocation.GetComponent<BoxCollider2D>().transform.position.y + Random.Range(-fieldOfLocation.GetComponent<BoxCollider2D>().size.y / 2, fieldOfLocation.GetComponent<BoxCollider2D>().size.y / 2));
+           
+            waitUntil = Time.time + waitTimeLength;
+        }
+    }
+}
---
+++ Assets/Scripts/AI/Generic/NPCSpawner.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPCSpawner : MonoBehaviour
+{
+    [SerializeField]
+    float spawnTimeLength;
+    [SerializeField]
+    float spawnUntil;
+    [SerializeField]
+    int numberToSpawn;
+    [SerializeField]
+    int numberOfSpawnLeft;
+
+    List<GameObject> maxNPC;
+
+    [SerializeField]
+    GameObject drunkNPC, aggressiveNPC, passiveNPC, location;
+
+    void Start()
+    {
+        drunkNPC.GetComponent<GenericNPCMovement>().fieldOfLocation = location;
+        aggressiveNPC.GetComponent<GenericNPCMovement>().fieldOfLocation = location;
+        passiveNPC.GetComponent<GenericNPCMovement>().fieldOfLocation = location;
+
+        maxNPC = new List<GameObject>();
+    }
+
+    void Update()
+    {
+        if(maxNPC.Count < numberToSpawn)
+        {
+            Spawn();
+        }
+
+        
+    }
+
+    void Spawn()
+    {
+        for(int i = 0; i < numberToSpawn; i++)
+        {
+            if(Time.time > spawnUntil)
+            {
+                int randomNum = (int)Random.Range(1f, 10f);
+                if(randomNum == 1)
+                {
+                    Instantiate(drunkNPC, this.transform.position, this.transform.rotation);
+                    maxNPC.Add(drunkNPC);
+                }
+                if (randomNum > 1 && randomNum <= 4)
+                {
+                    Instantiate(aggressiveNPC, this.transform.position, this.transform.rotation);
+                    maxNPC.Add(aggressiveNPC);
+                }
+                if (randomNum > 4 && randomNum <= 10)
+                {
+                    Instantiate(passiveNPC, this.transform.position, this.transform.rotation);
+                    maxNPC.Add(passiveNPC);
+                }
+                spawnUntil = Time.time + spawnTimeLength;
+                numberOfSpawnLeft++;
+            }
+        }
+    }
+}
