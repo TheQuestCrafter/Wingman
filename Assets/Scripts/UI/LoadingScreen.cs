6e47d5e63116df0eb8ec750894c6cecc6e672004
KeeganJennings <34640514+KeeganJennings@users.noreply.github.com>
2019-06-02 15:20:14 -0500

    This update adds the loading screen

---
+++ Assets/Scripts/UI/LoadingScreen.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadingScreen : MonoBehaviour
+{
+    private bool isVisibleOnCam;
+    private bool anyKeyPressed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        isVisibleOnCam = true;
+        anyKeyPressed = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CheckForKey();
+        DestroyLoadingScreen();
+    }
+
+    private void CheckForKey()
+    {
+        if(Input.anyKey)
+        {
+            anyKeyPressed = true;
+        }
+    }
+
+    private void DestroyLoadingScreen()
+    {
+        if (anyKeyPressed)
+        {
+            if (this.GetComponent<SpriteRenderer>().color.a > 0)
+            {
+                this.GetComponent<SpriteRenderer>().color -= Color.black * 0.01f;
+            }
+            else
+            {
+                Destroy(this.gameObject, 6.0f);
+            }
+        }
+    }
+
+    public bool returnVisibility()
+    {
+        return isVisibleOnCam;
+    }
+}
