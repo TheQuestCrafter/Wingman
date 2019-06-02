2c704e8b1da56ae5a2a44c3e8754cf5467db8a01
CalvinOh <calvinohwyekit@gmail.com>
2019-06-02 16:20:36 -0500

    Lover Prefab made, Lover Spawner

---
+++ Assets/Scripts/AI/Couples/LoversSpawn.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoversSpawn : MonoBehaviour
+{
+    [SerializeField]
+    List<GameObject> Lovers;
+
+    void Awake()
+    {
+        
+    }
+
+    void Spawn()
+    {
+        foreach(GameObject l in Lovers)
+        {
+            Instantiate(l, this.transform.position, this.transform.rotation);
+        }
+    }
+}
